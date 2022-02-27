61fb732d4961e0942326ae52e1d00a01fca073ac
AntonKokhno <antonkokhno1392@gmail.com>
2022-02-28 02:02:55 +0300

    add task 6 and task 7

---
+++ src/task6/Task6.java
@@ -0,0 +1,15 @@
+package task6;
+
+public class Task6 {
+    public static void main(String[] args) {
+         int s=46565657;
+         int se = s%10;
+         boolean sed = se==7;
+         System.out.println(sed);
+
+
+        }
+
+
+
+    }
