37e0835c124f05bfa0bf382195694e14c161c7be
AntonKokhno <antonkokhno1392@gmail.com>
2022-02-28 20:19:31 +0300

    changing variable input via the consoleaaas

---
+++ src/task10/Task10.java
@@ -0,0 +1,27 @@
+package task10;
+
+public class Task10 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 6;
+        int c = 15;
+        int d = 10;
+        int e = 30;
+        int f = 45;
+       if (a>b){
+           else if (c>d);
+           else if ()
+       }
+
+        }
+        int square1 = a * b;
+        int square2 = c * d;
+
+        int square3 = f * d;
+        if (square3 >= (square1 + square2)) {
+            System.out.println("Дома поместятся на участок");
+        } else
+            System.out.println("Дома не поместятся на участок");
+    }
+}
+
--- src/task6/Task6.java
+++ src/task6/Task6.java
@@ -6,26 +6,15 @@ public class Task6 {
     public static void main(String[] args) {
         Scanner f = new Scanner(System.in);
         System.out.println("Введите число");
-        int w =f.nextInt();
+        int w = f.nextInt();
 
-         int se = w%10;
-         boolean sed = se==7;
-         System.out.println(sed);
-         if(sed){
-             System.out.println(" заданное значение "+ w + " содержит на конце цифру 7");
-         }
-         else{
-             System.out.println(" заданное значение "+ w + " не содержит на конце цифру 7");
-         }
-
-
-
-
-
-
-
+        int se = w % 10;
+        boolean sed = se == 7;
+        System.out.println(sed);
+        if (sed) {
+            System.out.println(" заданное значение " + w + " содержит на конце цифру 7");
+        } else {
+            System.out.println(" заданное значение " + w + " не содержит на конце цифру 7");
         }
-
-
-
     }
+}
