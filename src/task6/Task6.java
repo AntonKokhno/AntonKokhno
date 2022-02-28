fb311eee34a2204f394cee2f27add4dcde36e8a2
AntonKokhno <antonkokhno1392@gmail.com>
2022-02-28 12:59:28 +0300

    changing variable input via the console

--- src/task6/Task6.java
+++ src/task6/Task6.java
@@ -1,11 +1,27 @@
 package task6;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-         int s=46565657;
-         int se = s%10;
+        Scanner f = new Scanner(System.in);
+        System.out.println("Введите число");
+        int w =f.nextInt();
+
+         int se = w%10;
          boolean sed = se==7;
          System.out.println(sed);
+         if(sed){
+             System.out.println(" заданное значение "+ w + " содержит на конце цифру 7");
+         }
+         else{
+             System.out.println(" заданное значение "+ w + " не содержит на конце цифру 7");
+         }
+
+
+
+
+
 
 
         }
